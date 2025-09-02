6718eb3738be98e3831860c07e891890aaad8b4d
maxlanionunes <46209614+maxlanionunes@users.noreply.github.com>
2025-09-02 00:24:54 +0000

    010920025

---
+++ .vscode/n2r_nc4_1.c/n2r_nc4_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int opcao;
+
+    printf("Escolha uma opção: \n");
+    printf("1. Verificar Saldo: \n");
+    printf("2. Fazer Depósito: \n");
+    printf("3. Fazer saque: \n");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case constant expression:
+        /* code */
+        break;
+    
+    case valor2;
+        /*code*/
+        break;
+    
+    default:
+        break;
+    }
+
+}
